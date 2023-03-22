a868d10cb7e1e0c41450e06d85343cac28c2a408
gmarquet <gmarquet@talend.com>
2023-03-22 11:16:42 +0100

    Implement CODEOWNERS detection and/or errors
    Signed-off-by: gmarquet <gmarquet@talend.com>

---
+++ pkg/policies/codeowners/codeowners.go
@@ -0,0 +1,240 @@
+// Copyright 2023 Allstar Authors
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package codeowners implements the CODEOWNERS policy.
+package codeowners
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ossf/allstar/pkg/config"
+	"github.com/ossf/allstar/pkg/policydef"
+
+	"github.com/google/go-github/v50/github"
+	"github.com/rs/zerolog/log"
+)
+
+const configFile = "codeowners.yaml"
+const polName = "CODEOWNERS"
+
+const notifyText = `A CODEOWNERS file can give users information about who is responsible for the maintenance of the repository, or specific folders/files. This is different the access control/permissions on a repository.
+
+To fix this, add a CODEOWNERS file to your repository, following the official Github documentation and maybe your company's policy.
+https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners`
+
+// OrgConfig is the org-level config definition for Branch Protection.
+type OrgConfig struct {
+	// OptConfig is the standard org-level opt in/out config, RepoOverride applies to all
+	// BP config.
+	OptConfig config.OrgOptConfig `json:"optConfig"`
+
+	// Action defines which action to take, default log, other: issue...
+	Action string `json:"action"`
+
+	//TODO add default contents for "fix" action
+}
+
+// RepoConfig is the repo-level config for Branch Protection
+type RepoConfig struct {
+	// OptConfig is the standard repo-level opt in/out config.
+	OptConfig config.RepoOptConfig `json:"optConfig"`
+
+	// Action overrides the same setting in org-level, only if present.
+	Action *string `json:"action"`
+}
+
+type mergedConfig struct {
+	Action string
+}
+
+type details struct {
+	Enabled         bool
+	URL             string
+	CodeownersFound bool
+	ErrorCount      int
+}
+
+var configFetchConfig func(context.Context, *github.Client, string, string, string, config.ConfigLevel, interface{}) error
+
+var configIsEnabled func(ctx context.Context, o config.OrgOptConfig, orc, r config.RepoOptConfig, c *github.Client, owner, repo string) (bool, error)
+
+func init() {
+	configFetchConfig = config.FetchConfig
+	configIsEnabled = config.IsEnabled
+}
+
+type repositories interface {
+	GetCodeownersErrors(context.Context, string, string) ([]*github.CodeownersErrors, *github.Response, error)
+}
+
+// Codeowners is the CODEOWNERS policy object, implements policydef.Policy.
+type Codeowners bool
+
+// NewCodeowners returns a new CODEOWNERS policy.
+func NewCodeowners() policydef.Policy {
+	var s Codeowners
+	return s
+}
+
+// Name returns the name of this policy, implementing policydef.Policy.Name()
+func (s Codeowners) Name() string {
+	return polName
+}
+
+// Check performs the policy check for CODEOWNERS policy based on the
+// configuration stored in the org/repo, implementing policydef.Policy.Check()
+func (s Codeowners) Check(ctx context.Context, c *github.Client, owner,
+	repo string) (*policydef.Result, error) {
+	return check(ctx, *c.Repositories, c, owner, repo)
+}
+
+// Check whether this policy is enabled or not
+func (s Codeowners) IsEnabled(ctx context.Context, c *github.Client, owner, repo string) (bool, error) {
+	oc, orc, rc := getConfig(ctx, c, owner, repo)
+	return configIsEnabled(ctx, oc.OptConfig, orc.OptConfig, rc.OptConfig, c, owner, repo)
+}
+
+func check(ctx context.Context, rep github.RepositoriesService, c *github.Client, owner,
+	repo string) (*policydef.Result, error) {
+	oc, orc, rc := getConfig(ctx, c, owner, repo)
+	enabled, err := configIsEnabled(ctx, oc.OptConfig, orc.OptConfig, rc.OptConfig, c, owner, repo)
+	var d details
+	d.Enabled = enabled
+	d.URL = ""
+	if err != nil {
+		return nil, err
+	}
+	log.Info().
+		Str("org", owner).
+		Str("repo", repo).
+		Str("area", polName).
+		Bool("enabled", enabled).
+		Msg("Check repo enabled")
+
+	codeownererrors, _, err := rep.GetCodeownersErrors(context.Background(), owner, repo)
+
+	// this is a 404, CODEOWNERS is not the in the repository
+	if err != nil {
+		d.CodeownersFound = false
+		return &policydef.Result{
+			Enabled:    enabled,
+			Pass:       false,
+			NotifyText: "CODEOWNERS file not present.\n" + notifyText,
+			Details:    d,
+		}, nil
+	}
+
+	d.ErrorCount = len(codeownererrors.Errors)
+	d.CodeownersFound = true
+	// the CODEOWNERS is present and has no errors, pass
+	if d.ErrorCount == 0 {
+		return &policydef.Result{
+			Enabled:    enabled,
+			Pass:       true,
+			NotifyText: "",
+			Details:    d,
+		}, nil
+	}
+	// otherwise, fail because CODEOWNERS has errors
+	var errorMessage = fmt.Sprintf("CODEOWNERS file present but has %d errors.\n", d.ErrorCount)
+	for _, e := range codeownererrors.Errors {
+		errorMessage += fmt.Sprintf("- %s\n  - %s", e.Path, e.Message)
+	}
+	return &policydef.Result{
+		Enabled:    enabled,
+		Pass:       false,
+		NotifyText: errorMessage,
+		Details:    d,
+	}, nil
+
+}
+
+// Fix implementing policydef.Policy.Fix(). Currently not supported. Plan
+// to support this TODO.
+func (s Codeowners) Fix(ctx context.Context, c *github.Client, owner, repo string) error {
+	log.Warn().
+		Str("org", owner).
+		Str("repo", repo).
+		Str("area", polName).
+		Msg("Action fix is configured, but not implemented.")
+	return nil
+}
+
+// GetAction returns the configured action from CODEOWNERS policy's
+// configuration stored in the org-level repo, default log. Implementing
+// policydef.Policy.GetAction()
+func (s Codeowners) GetAction(ctx context.Context, c *github.Client, owner, repo string) string {
+	oc, orc, rc := getConfig(ctx, c, owner, repo)
+	mc := mergeConfig(oc, orc, rc, repo)
+	return mc.Action
+}
+
+func getConfig(ctx context.Context, c *github.Client, owner, repo string) (*OrgConfig, *RepoConfig, *RepoConfig) {
+	oc := &OrgConfig{ // Fill out non-zero defaults
+		Action: "log",
+	}
+	if err := configFetchConfig(ctx, c, owner, "", configFile, config.OrgLevel, oc); err != nil {
+		log.Error().
+			Str("org", owner).
+			Str("repo", repo).
+			Str("configLevel", "orgLevel").
+			Str("area", polName).
+			Str("file", configFile).
+			Err(err).
+			Msg("Unexpected config error, using defaults.")
+	}
+	orc := &RepoConfig{}
+	if err := configFetchConfig(ctx, c, owner, repo, configFile, config.OrgRepoLevel, orc); err != nil {
+		log.Error().
+			Str("org", owner).
+			Str("repo", repo).
+			Str("configLevel", "orgRepoLevel").
+			Str("area", polName).
+			Str("file", configFile).
+			Err(err).
+			Msg("Unexpected config error, using defaults.")
+	}
+	rc := &RepoConfig{}
+	if err := configFetchConfig(ctx, c, owner, repo, configFile, config.RepoLevel, rc); err != nil {
+		log.Error().
+			Str("org", owner).
+			Str("repo", repo).
+			Str("configLevel", "repoLevel").
+			Str("area", polName).
+			Str("file", configFile).
+			Err(err).
+			Msg("Unexpected config error, using defaults.")
+	}
+	return oc, orc, rc
+}
+
+func mergeConfig(oc *OrgConfig, orc *RepoConfig, rc *RepoConfig, repo string) *mergedConfig {
+	mc := &mergedConfig{
+		Action: oc.Action,
+	}
+	mc = mergeInRepoConfig(mc, orc, repo)
+
+	if !oc.OptConfig.DisableRepoOverride {
+		mc = mergeInRepoConfig(mc, rc, repo)
+	}
+	return mc
+}
+
+func mergeInRepoConfig(mc *mergedConfig, rc *RepoConfig, repo string) *mergedConfig {
+	if rc.Action != nil {
+		mc.Action = *rc.Action
+	}
+	return mc
+}
---
+++ pkg/policies/codeowners/codeowners_test.go
@@ -0,0 +1,259 @@
+// Copyright 2023 Allstar Authors
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package codeowners
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-github/v50/github"
+	"github.com/ossf/allstar/pkg/config"
+	"github.com/ossf/allstar/pkg/policydef"
+)
+
+var query func(context.Context, interface{}, map[string]interface{}) error
+
+type mockClient struct{}
+
+func (m mockClient) Query(ctx context.Context, q interface{}, v map[string]interface{}) error {
+	return query(ctx, q, v)
+}
+
+func TestConfigPrecedence(t *testing.T) {
+	tests := []struct {
+		Name      string
+		Org       OrgConfig
+		OrgRepo   RepoConfig
+		Repo      RepoConfig
+		ExpAction string
+		Exp       mergedConfig
+	}{
+		{
+			Name: "OrgOnly",
+			Org: OrgConfig{
+				Action: "issue",
+			},
+			OrgRepo:   RepoConfig{},
+			Repo:      RepoConfig{},
+			ExpAction: "issue",
+			Exp: mergedConfig{
+				Action: "issue",
+			},
+		},
+		{
+			Name: "OrgRepoOverOrg",
+			Org: OrgConfig{
+				Action: "issue",
+			},
+			OrgRepo: RepoConfig{
+				Action: github.String("log"),
+			},
+			Repo:      RepoConfig{},
+			ExpAction: "log",
+			Exp: mergedConfig{
+				Action: "log",
+			},
+		},
+		{
+			Name: "RepoOverAllOrg",
+			Org: OrgConfig{
+				Action: "issue",
+			},
+			OrgRepo: RepoConfig{
+				Action: github.String("log"),
+			},
+			Repo: RepoConfig{
+				Action: github.String("email"),
+			},
+			ExpAction: "email",
+			Exp: mergedConfig{
+				Action: "email",
+			},
+		},
+		{
+			Name: "RepoDisallowed",
+			Org: OrgConfig{
+				OptConfig: config.OrgOptConfig{
+					DisableRepoOverride: true,
+				},
+				Action: "issue",
+			},
+			OrgRepo: RepoConfig{
+				Action: github.String("log"),
+			},
+			Repo: RepoConfig{
+				Action: github.String("email"),
+			},
+			ExpAction: "log",
+			Exp: mergedConfig{
+				Action: "log",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			configFetchConfig = func(ctx context.Context, c *github.Client,
+				owner, repo, path string, ol config.ConfigLevel, out interface{}) error {
+				switch ol {
+				case config.RepoLevel:
+					rc := out.(*RepoConfig)
+					*rc = test.Repo
+				case config.OrgRepoLevel:
+					orc := out.(*RepoConfig)
+					*orc = test.OrgRepo
+				case config.OrgLevel:
+					oc := out.(*OrgConfig)
+					*oc = test.Org
+				}
+				return nil
+			}
+
+			s := Codeowners(true)
+			ctx := context.Background()
+
+			action := s.GetAction(ctx, nil, "", "thisrepo")
+			if action != test.ExpAction {
+				t.Errorf("Unexpected results. want %s, got %s", test.ExpAction, action)
+			}
+
+			oc, orc, rc := getConfig(ctx, nil, "", "thisrepo")
+			mc := mergeConfig(oc, orc, rc, "thisrepo")
+			if diff := cmp.Diff(&test.Exp, mc); diff != "" {
+				t.Errorf("Unexpected results. (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestCheck(t *testing.T) {
+	tests := []struct {
+		Name         string
+		Org          OrgConfig
+		Repo         RepoConfig
+		SecEnabled   bool
+		cofigEnabled bool
+		Exp          policydef.Result
+	}{
+		{
+			Name:         "NotEnabled",
+			Org:          OrgConfig{},
+			Repo:         RepoConfig{},
+			SecEnabled:   true,
+			cofigEnabled: false,
+			Exp: policydef.Result{
+				Enabled:    false,
+				Pass:       true,
+				NotifyText: "",
+				Details: details{
+					Enabled: true,
+					URL:     "",
+				},
+			},
+		},
+		{
+			Name: "Pass",
+			Org: OrgConfig{
+				OptConfig: config.OrgOptConfig{
+					OptOutStrategy: true,
+				},
+			},
+			Repo:         RepoConfig{},
+			SecEnabled:   true,
+			cofigEnabled: true,
+			Exp: policydef.Result{
+				Enabled:    true,
+				Pass:       true,
+				NotifyText: "",
+				Details: details{
+					Enabled: true,
+					URL:     "",
+				},
+			},
+		},
+		{
+			Name: "Fail",
+			Org: OrgConfig{
+				OptConfig: config.OrgOptConfig{
+					OptOutStrategy: true,
+				},
+			},
+			Repo:         RepoConfig{},
+			SecEnabled:   false,
+			cofigEnabled: true,
+			Exp: policydef.Result{
+				Enabled: true,
+				Pass:    false,
+				NotifyText: "CODEOWNERS policy not enabled.\n" + `A CODEOWNERS file can give users information about who is responsible for the maintenance of the repository, or specific folders/files. This is different the access control/permissions on a repository.
+
+				To fix this, add a CODEOWNERS file to your repository, following the official Github documentation and maybe your company's policy.
+				https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners`,
+				Details: details{
+					Enabled: false,
+					URL:     "",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			configFetchConfig = func(ctx context.Context, c *github.Client,
+				owner, repo, path string, ol config.ConfigLevel, out interface{}) error {
+				if repo == "thisrepo" && ol == config.RepoLevel {
+					rc := out.(*RepoConfig)
+					*rc = test.Repo
+				} else if ol == config.OrgLevel {
+					oc := out.(*OrgConfig)
+					*oc = test.Org
+				}
+				return nil
+			}
+			query = func(ctx context.Context, q interface{}, v map[string]interface{}) error {
+				qc, ok := q.(*struct {
+					Repository struct {
+						SecurityPolicyUrl       string
+						IsSecurityPolicyEnabled bool
+					} `graphql:"repository(owner: $owner, name: $name)"`
+				})
+				if !ok {
+					t.Errorf("Query() called with unexpected query structure.")
+				}
+				qc.Repository.IsSecurityPolicyEnabled = test.SecEnabled
+				return nil
+			}
+			configIsEnabled = func(ctx context.Context, o config.OrgOptConfig, orc, r config.RepoOptConfig,
+				c *github.Client, owner, repo string) (bool, error) {
+				return test.cofigEnabled, nil
+			}
+			res, err := check(context.Background(), nil, mockClient{}, "", "thisrepo")
+			if err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+			c := cmp.Comparer(func(x, y string) bool { return trunc(x, 40) == trunc(y, 40) })
+			if diff := cmp.Diff(&test.Exp, res, c); diff != "" {
+				t.Errorf("Unexpected results. (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func trunc(s string, n int) string {
+	if n >= len(s) {
+		return s
+	}
+	return s[:n]
+}
